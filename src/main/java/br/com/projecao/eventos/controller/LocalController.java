fed249bcf605b78f0250c0bc0384d2ed7bee14e6
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-04 13:52:07 -0300

    Update LocalController.java

--- src/main/java/br/com/projecao/eventos/controller/LocalController.java
+++ src/main/java/br/com/projecao/eventos/controller/LocalController.java
@@ -1,8 +1,15 @@
 package br.com.projecao.eventos.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import br.com.projecao.eventos.dto.LocalDTO;
 import br.com.projecao.eventos.service.LocalService;
+import jakarta.validation.Valid;
 
 @Controller
 public class LocalController {
@@ -11,9 +18,22 @@ public class LocalController {
 	public LocalController(LocalService localService) {
 		this.localService = localService;
 	}
-
+	
+	@GetMapping("/cadastrar/local")
+	public String formulario(Model model) {
+		model.addAttribute("localDTO", new LocalDTO());
+		return "local_form";
+	}
+	
+	@PostMapping("/cadastrar/local")
+	public String salvarLocal(@ModelAttribute("localDTO") @Valid LocalDTO localDTO, BindingResult result) {
+		if(result.hasErrors()){
+			return "local_form";
+		}
+		localService.salvarLocal(localDTO);
+		return "redirect:/cadastrar/local";
+	}
 }
 /*
-- POST /locais → cadastrar um novo local
 - GET /locais → listar todos os locais
  */

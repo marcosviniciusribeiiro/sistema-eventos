4ff1b3b5c4c6dc89bffb66cfbeee69e0d2b8b8b4
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-04 13:09:34 -0300

    Update LocalController.java

--- src/main/java/br/com/projecao/eventos/controller/LocalController.java
+++ src/main/java/br/com/projecao/eventos/controller/LocalController.java
@@ -2,8 +2,15 @@ package br.com.projecao.eventos.controller;
 
 import org.springframework.stereotype.Controller;
 
+import br.com.projecao.eventos.service.LocalService;
+
 @Controller
 public class LocalController {
+	private final LocalService localService;
+	
+	public LocalController(LocalService localService) {
+		this.localService = localService;
+	}
 
 }
 /*

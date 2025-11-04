4e78bf4a9c15518118bf90a0c5d2eb8ac7fabdff
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-04 11:45:40 -0300

    Create EventoService.java

---
+++ src/main/java/br/com/projecao/eventos/service/EventoService.java
@@ -0,0 +1,37 @@
+package br.com.projecao.eventos.service;
+
+import java.util.List;
+
+import br.com.projecao.eventos.dto.EventoDTO;
+import br.com.projecao.eventos.mapper.EventoMapper;
+import br.com.projecao.eventos.model.Evento;
+import br.com.projecao.eventos.repository.EventoRepository;
+
+public class EventoService {
+	private final EventoRepository eventoRepository;
+	
+	public EventoService(EventoRepository eventoRepository) {
+		this.eventoRepository = eventoRepository;
+	}
+	
+	public void salvarEvento(EventoDTO dto) {
+		Evento evento = EventoMapper.toEntity(dto);
+		eventoRepository.save(evento);
+	}
+	
+	public List<EventoDTO> listarEventos() {
+		return eventoRepository.findAll()
+							   .stream()
+							   .map(EventoMapper::toDTO)
+							   .toList();
+	}
+	
+	public EventoDTO buscarPorId(Long id) {
+		Evento evento = eventoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Id inválido: " + id));
+		return EventoMapper.toDTO(evento);
+	}
+	
+	public void excluirEvento(Long id) {
+		eventoRepository.deleteById(id);
+	}
+}

d677a2a22840c0a24b0903917cbcc13fd56d6849
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-04 13:09:37 -0300

    Update ParticipanteController.java

--- src/main/java/br/com/projecao/eventos/controller/ParticipanteController.java
+++ src/main/java/br/com/projecao/eventos/controller/ParticipanteController.java
@@ -2,9 +2,15 @@ package br.com.projecao.eventos.controller;
 
 import org.springframework.stereotype.Controller;
 
+import br.com.projecao.eventos.service.ParticipanteService;
+
 @Controller
 public class ParticipanteController {
-
+	private final ParticipanteService participanteService;
+	
+	public ParticipanteController(ParticipanteService participanteService) {
+		this.participanteService = participanteService;
+	}
 }
 /*
 - POST /participantes → cadastrar participante

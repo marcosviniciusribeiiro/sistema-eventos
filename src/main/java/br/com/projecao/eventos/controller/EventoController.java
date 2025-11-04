dda355bef0967fc51f5b53b526f0fa74856a8ec0
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-04 11:57:16 -0300

    Create EventoController.java

---
+++ src/main/java/br/com/projecao/eventos/controller/EventoController.java
@@ -0,0 +1,12 @@
+package br.com.projecao.eventos.controller;
+
+public class EventoController {
+
+}
+/*
+- POST /eventos → cadastrar um novo evento
+- GET /eventos → listar todos os eventos
+- GET /eventos/{id} → buscar evento por ID
+- PUT /eventos/{id} → atualizar informações do evento
+- DELETE /eventos/{id} → excluir evento
+ */

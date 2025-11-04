db3ed4dc87ab31e456c79e2f07079a875ebdf26a
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-04 11:48:13 -0300

    Update EventoService.java

--- src/main/java/br/com/projecao/eventos/service/EventoService.java
+++ src/main/java/br/com/projecao/eventos/service/EventoService.java
@@ -2,11 +2,14 @@ package br.com.projecao.eventos.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import br.com.projecao.eventos.dto.EventoDTO;
 import br.com.projecao.eventos.mapper.EventoMapper;
 import br.com.projecao.eventos.model.Evento;
 import br.com.projecao.eventos.repository.EventoRepository;
 
+@Service
 public class EventoService {
 	private final EventoRepository eventoRepository;
 	
@@ -27,7 +30,8 @@ public class EventoService {
 	}
 	
 	public EventoDTO buscarPorId(Long id) {
-		Evento evento = eventoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Id inválido: " + id));
+		Evento evento = eventoRepository.findById(id)
+										.orElseThrow(() -> new IllegalArgumentException("Id inválido: " + id));
 		return EventoMapper.toDTO(evento);
 	}
 	

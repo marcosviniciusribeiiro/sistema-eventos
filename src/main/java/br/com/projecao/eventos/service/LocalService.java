4f87470334bb07cfbb8e545069692e87dd47b03e
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-04 11:56:31 -0300

    Create LocalService.java

---
+++ src/main/java/br/com/projecao/eventos/service/LocalService.java
@@ -0,0 +1,31 @@
+package br.com.projecao.eventos.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.projecao.eventos.dto.LocalDTO;
+import br.com.projecao.eventos.mapper.LocalMapper;
+import br.com.projecao.eventos.model.Local;
+import br.com.projecao.eventos.repository.LocalRepository;
+
+@Service
+public class LocalService {
+	private LocalRepository localRepository;
+	
+	public LocalService(LocalRepository localRepository) {
+		this.localRepository = localRepository;
+	}
+	
+	public void salvarLocal(LocalDTO dto) {
+		Local local = LocalMapper.toEntity(dto);
+		localRepository.save(local);
+	}
+	
+	public List<LocalDTO> listarLocais() {
+		return localRepository.findAll()
+							  .stream()
+							  .map(LocalMapper::toDTO)
+							  .toList();
+	}
+}

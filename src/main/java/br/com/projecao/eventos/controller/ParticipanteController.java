7d992084708b245a83861c05287215e0790c8934
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-04 11:57:20 -0300

    Create ParticipanteController.java

---
+++ src/main/java/br/com/projecao/eventos/controller/ParticipanteController.java
@@ -0,0 +1,9 @@
+package br.com.projecao.eventos.controller;
+
+public class ParticipanteController {
+
+}
+/*
+- POST /participantes → cadastrar participante
+- GET /participantes → listar todos os participantes
+ */

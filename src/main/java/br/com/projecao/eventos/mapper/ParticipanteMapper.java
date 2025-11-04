92cb7a0b8c59ac44fe8a976c5b677f36c624dfc1
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-04 11:29:00 -0300

    Create ParticipanteMapper.java

---
+++ src/main/java/br/com/projecao/eventos/mapper/ParticipanteMapper.java
@@ -0,0 +1,22 @@
+package br.com.projecao.eventos.mapper;
+
+import br.com.projecao.eventos.dto.ParticipanteDTO;
+import br.com.projecao.eventos.model.Participante;
+
+public class ParticipanteMapper {
+	public static Participante toEntity(ParticipanteDTO dto) {
+		Participante participante = new Participante();
+		participante.setId(dto.getId());
+		participante.setNome(dto.getNome());
+		participante.setEmail(dto.getEmail());
+		return participante;
+	}
+	
+	public static ParticipanteDTO toDTO(Participante participante) {
+		ParticipanteDTO dto = new ParticipanteDTO();
+		dto.setId(participante.getId());
+		dto.setNome(participante.getNome());
+		dto.setEmail(participante.getEmail());
+		return dto;
+	}
+}

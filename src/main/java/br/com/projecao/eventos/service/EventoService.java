a5df5a2b3525b3d8a5a1b8f5c7f258f98361f12d
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-06 11:46:36 -0300

    Update EventoService.java

--- src/main/java/br/com/projecao/eventos/service/EventoService.java
+++ src/main/java/br/com/projecao/eventos/service/EventoService.java
@@ -7,18 +7,24 @@ import org.springframework.stereotype.Service;
 import br.com.projecao.eventos.dto.EventoDTO;
 import br.com.projecao.eventos.mapper.EventoMapper;
 import br.com.projecao.eventos.model.Evento;
+import br.com.projecao.eventos.model.Local;
 import br.com.projecao.eventos.repository.EventoRepository;
 
 @Service
 public class EventoService {
 	private final EventoRepository eventoRepository;
+	private final LocalService localService;
 	
-	public EventoService(EventoRepository eventoRepository) {
+	public EventoService(EventoRepository eventoRepository, LocalService localService) {
 		this.eventoRepository = eventoRepository;
+		this.localService = localService;
 	}
 	
 	public void salvarEvento(EventoDTO dto) {
 		Evento evento = EventoMapper.toEntity(dto);
+		Local local = localService.buscarEntidadePorId(dto.getLocalId());
+		
+		evento.setLocal(local);
 		eventoRepository.save(evento);
 	}
 	

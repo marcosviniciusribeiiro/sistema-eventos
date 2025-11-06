84e43cb6afcf32a6e3b64380f4fe2f8d03ce7355
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-06 10:46:22 -0300

    Update ParticipanteDTO.java

--- src/main/java/br/com/projecao/eventos/dto/ParticipanteDTO.java
+++ src/main/java/br/com/projecao/eventos/dto/ParticipanteDTO.java
@@ -1,5 +1,8 @@
 package br.com.projecao.eventos.dto;
 
+import java.util.List;
+
+import br.com.projecao.eventos.model.Evento;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
@@ -13,6 +16,8 @@ public class ParticipanteDTO {
 	@NotBlank(message = "O e-mail do participante é obrigatório!")	
 	private String email;
 
+	private List<Evento> eventos;
+	
 	public Long getId() {
 		return id;
 	}
@@ -35,5 +40,13 @@ public class ParticipanteDTO {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Evento> getEventos() {
+		return eventos;
+	}
+
+	public void setEventos(List<Evento> eventos) {
+		this.eventos = eventos;
 	}
 }

f3d1c789d4dae99724a51179349ad7e9d07e9469
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-11-06 11:46:51 -0300

    Update EventoMapper.java

--- src/main/java/br/com/projecao/eventos/mapper/EventoMapper.java
+++ src/main/java/br/com/projecao/eventos/mapper/EventoMapper.java
@@ -1,15 +1,19 @@
 package br.com.projecao.eventos.mapper;
 
+//import java.util.List;
+
 import br.com.projecao.eventos.dto.EventoDTO;
 import br.com.projecao.eventos.model.Evento;
+//import br.com.projecao.eventos.model.Participante;
 
 public class EventoMapper {
-	public static Evento toEntity(EventoDTO dto) {
+	public static Evento toEntity(EventoDTO dto/*, List<Participante> participantes*/) {
 		Evento evento = new Evento();
 		evento.setId(dto.getId());
 		evento.setNome(dto.getNome());
 		evento.setDescricao(dto.getDescricao());
 		evento.setData(dto.getData());
+//		evento.setParticipantes(participantes);
 		return evento;
 	}
 	public static EventoDTO toDTO(Evento evento) {
@@ -18,6 +22,11 @@ public class EventoMapper {
 		dto.setNome(evento.getNome());
 		dto.setDescricao(evento.getDescricao());
 		dto.setData(evento.getData());
+	//	dto.setParticipantesIds(evento.getParticipantes().stream().map(Participante::getId).toList());
+		if(evento.getLocal() != null) {
+			dto.setLocalId(evento.getLocal().getId());
+			dto.setLocalNome(evento.getLocal().getNome());
+		}
 		return dto;
 	}
 }
